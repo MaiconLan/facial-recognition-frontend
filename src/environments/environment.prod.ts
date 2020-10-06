689aa1b94f86b0826492544840a4fe938d5e21fd
Maicon Lanzendorf <maicon.lanzendorf@live.com>
2020-10-05 23:42:50 -0300

    testes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'https://aw-facial-recognition.herokuapp.com/reconhecimento',
-  allowedDomains: [/aw-facial-recognition.herokuapp.com/],
+  allowedDomains: [/aw-facial-recognition.herokuapp.com\/reconhecimento/],
   disallowedRoutes: [/\/oauth\/token/]
 };
