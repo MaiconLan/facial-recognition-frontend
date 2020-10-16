ae319da081984c80914bb44c78c8d2ba2024046e
Renan Zapelini <renanzapelini27@gmail.com>
2020-10-15 23:09:55 -0300

    acessado as opções do backend para quantidade de alunos e professores cadastrados

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,35 +1,157 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {map} from 'rxjs/operators';
 import {Breakpoints, BreakpointObserver} from '@angular/cdk/layout';
+import {DashboardService} from './dashboard.service';
+
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
   /** Based on the screen size, switch from standard to one column per row */
+
+  cardsPc = [
+    {
+      id: 1,
+      title: 'Alunos sem Foto',
+      cols: 2,
+      rows: 1,
+      mostrarModal: false,
+      style: {'background-color': '#CB4335', 'color': '#fff'},
+      numero: '0',
+      link: '/aluno'
+    },
+    {
+      id: 2,
+      title: 'Alunos cadastrados',
+      cols: 1,
+      rows: 1,
+      mostrarModal: false,
+      style: {'background-color': '#229954', 'color': 'white'},
+      numero: '0',
+      link: '/professor'
+    },
+    {
+      id: 3,
+      title: 'Profs. cadastrados',
+      cols: 2,
+      rows: 1,
+      mostrarModal: false,
+      style: {'background-color': '#7D3C98', 'color': '#fff'},
+      numero: '0'
+    },
+    {
+      id: 4,
+      title: 'Aulas do dia',
+      cols: 1,
+      rows: 1,
+      mostrarModal: false,
+      style: {'background-color': '#2E4053', 'color': '#fff'},
+      numero: '0'
+    }
+  ];
+
+  cardsMobile = [
+    {
+      id: 1,
+      title: 'Alunos sem Foto',
+      cols: 1,
+      rows: 1,
+      mostrarModal: false,
+      style: {'background-color': '#CB4335', 'color': 'white'},
+      numero: '0',
+      link: '/aluno'
+    },
+    {
+      id: 2,
+      title: 'Alunos cadastrados',
+      cols: 1,
+      rows: 1,
+      mostrarModal: false,
+      style: {'background-color': '#229954', 'color': 'white'},
+      numero: '0',
+      link: '/professor'
+    },
+    {
+      id: 3,
+      title: 'Profs. cadastrados',
+      cols: 1,
+      rows: 1,
+      mostrarModal: false,
+      style: {'background-color': '#7D3C98', 'color': '#fff'},
+      numero: '0'
+    },
+    {
+      id: 4,
+      title: 'Aulas do dia',
+      cols: 1,
+      rows: 1,
+      mostrarModal: false,
+      style: {'background-color': '#2E4053', 'color': '#fff'},
+      numero: '0'
+    }
+  ];
+
   cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
     map(({matches}) => {
       // mobile
       if (matches) {
-        return [
-          {title: 'Alunos sem Foto', cols: 1, rows: 1, style: {'background-color': '#CB4335', 'color': 'white'}, numero: '9', link: "/aluno"},
-          {title: 'Alunos cadastrados', cols: 1, rows: 1, style: {'background-color': '#229954', 'color': 'white'}, numero: '10', link: "/professor" },
-          {title: 'Profs. cadastrados', cols: 1, rows: 1, style: {'background-color': '#7D3C98', 'color': '#fff'}, numero: '11'},
-          {title: 'Aulas do dia', cols: 1, rows: 1, style: {'background-color': '#2E4053', 'color': '#fff'}, numero: '12'}
-        ];
+        return this.cardsMobile;
       }
 
-      return [
-        {title: 'Alunos sem Foto', cols: 2, rows: 1, style: {'background-color': '#CB4335', 'color': '#fff'}, numero: '9',  link: "/aluno"},
-        {title: 'Alunos cadastrados', cols: 1, rows: 1, style: {'background-color': '#229954', 'color': 'white'}, numero: '10', link: "/professor"},
-        {title: 'Profs. cadastrados', cols: 2, rows: 1, style: {'background-color': '#7D3C98', 'color': '#fff'}, numero: '11'},
-        {title: 'Aulas do dia', cols: 1, rows: 1, style: {'background-color': '#2E4053', 'color': '#fff'}, numero: '12'}
-      ];
+      return this.cardsPc;
     })
   );
 
-  constructor(private breakpointObserver: BreakpointObserver) {
+  constructor(private breakpointObserver: BreakpointObserver,
+              private dashboardService: DashboardService) {
+  }
+
+  abrirModalDashboard(card: any): void {
+    card.mostrarModal = true;
+  }
+
+  fecharModalDashboard(card: any): void {
+    card.mostrarModal = false;
+  }
+
+  ngOnInit(): void {
+    for (const card of this.cardsPc) {
+      if (card.id === 2) {
+        this.dashboardService.getAlunoDashboard()
+          .then(response => {
+            card.numero = response.alunosCadastrados;
+          });
+      }
+    }
+
+    for (const card of this.cardsMobile) {
+      if (card.id === 2) {
+        this.dashboardService.getAlunoDashboard()
+          .then(response => {
+            card.numero = response.alunosCadastrados;
+          });
+      }
+    }
+
+    for (const card of this.cardsPc) {
+      if (card.id === 3) {
+        this.dashboardService.getProfessorDashboard()
+          .then(response => {
+            card.numero = response.professoresCadastrados;
+          });
+      }
+    }
+
+    for (const card of this.cardsMobile) {
+      if (card.id === 3) {
+        this.dashboardService.getProfessorDashboard()
+          .then(response => {
+            card.numero = response.professoresCadastrados;
+          });
+      }
+    }
   }
 }
---
+++ src/app/dashboard/dashboard.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {environment} from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DashboardService {
+
+  urlAluno = environment.apiUrl + '/aluno/dashboard';
+  urlProfessor = environment.apiUrl + '/professor/dashboard';
+
+  constructor(private http: HttpClient) { }
+
+  getAlunoDashboard(): Promise<any> {
+    const headers = new HttpHeaders()
+      .append('Content-Type', 'application/json');
+
+    return this.http.get<any>(`${this.urlAluno}`, {headers})
+      .toPromise()
+      .then()
+      .catch(error => {
+        return Promise.reject(error);
+      });
+  }
+
+  getProfessorDashboard(): Promise<any> {
+    const headers = new HttpHeaders()
+      .append('Content-Type', 'application/json');
+
+    return this.http.get<any>(`${this.urlProfessor}`, {headers})
+      .toPromise()
+      .then()
+      .catch(error => {
+        return Promise.reject(error);
+      });
+  }
+}
